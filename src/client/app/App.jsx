86eae940e12298c91736469eab877661fbddbc52
Mahadev <v.mahadev@gmail.com>
2016-08-26 00:01:31 +0000

    init2

---
+++ src/client/app/App.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import AwesomeComponent from './AwesomeComponent.jsx';
+
+class App extends React.Component {
+   render() {
+      return (
+         <div>
+            <p>Hello World!!!</p>
+            <AwesomeComponent/>
+         </div>
+      );
+   }
+}
+
+export default App;
+
